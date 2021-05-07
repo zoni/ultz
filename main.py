474fd386db4fee7f11634b5953b524263cda02a0
Nick Groenen <nick@groenen.me>
2021-05-07 12:31:25 +0200

    Workaround Gtk3/4 incompatibility

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import gi; gi.require_version('Gdk', '3.0') # https://github.com/Ulauncher/Ulauncher/issues/703
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.shared.action.DoNothingAction import DoNothingAction
